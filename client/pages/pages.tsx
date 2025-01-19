af7180c7d10d5733fe4631eddfe2d9759a1832c3
shivampandey404 <shivampandey242524@gmail.com>
2025-01-20 01:40:43 +0530

    update

---
+++ client/pages/pages.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Custom404: React.FC = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, we couldn’t find the page you’re looking for.</p>
+    </div>
+  );
+};
+
+export default Custom404;
